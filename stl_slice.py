a67e85d57af4d37510430928a65d0eb09b4db89a
KitchNasty02 <connorkitch10@gmail.com>
2026-02-07 23:42:29 -0600

    added cube interlock in y and z directions

--- stl_slice.py
+++ stl_slice.py
@@ -14,7 +14,7 @@ def split_mesh(mesh):
         mesh: The mesh to be split
 
     Returns:
-        Two mesh objects and centroid of original mesh
+        Two mesh objects
         
     """
     centroid = mesh.centroid
@@ -46,7 +46,7 @@ def split_mesh(mesh):
     # print(f'Is left watertight: {mesh_left.is_watertight}')
     # print(f'Is right watertight: {mesh_right.is_watertight}')
 
-    return mesh_left, mesh_right, centroid
+    return mesh_left, mesh_right
 
 
 
@@ -79,15 +79,15 @@ def get_split_face(mesh):
     cut_width = max_y - min_y
     cut_height = max_z - min_z
 
-    print(f'Cutface width (Y): {cut_width:.2f} mm')
-    print(f'Cutface height (Z): {cut_height:.2f} mm')
+    # print(f'Cutface width (Y): {cut_width:.2f} mm')
+    # print(f'Cutface height (Z): {cut_height:.2f} mm')
 
     return cut_width, cut_height, (min_y, max_y, min_z, max_z)
 
 
 
 
-def add_cube_interlock(mesh_left, mesh_right, beam_width_layers=2, beam_height_layers=2, beam_depth_layers=2, avoidance_dist=NOZZLE_SIZE):
+def add_cube_interlock_in_z(mesh_left, mesh_right, beam_width_layers=2, beam_height_layers=2, beam_depth_layers=2, avoidance_dist=NOZZLE_SIZE):
     """
     Adds a cube-based interlock between two mesh halves
     
@@ -117,9 +117,16 @@ def add_cube_interlock(mesh_left, mesh_right, beam_width_layers=2, beam_height_l
     # print(f'int width: {interlock_width}')
 
     num_int_vertical = np.floor(interlock_height / (beam_height_layers*LAYER_HEIGHT))
+
+    # if the num is within the tolerance of a cube, then go ahead and add it
+    if np.ceil(num_int_vertical) - num_int_vertical < 0.25:
+        num_int_vertical = np.ceil(num_int_vertical)
+    else:
+        num_int_vertical = np.floor(num_int_vertical)
+
     int_vertical_height = np.round(num_int_vertical * (beam_height_layers*LAYER_HEIGHT), 3) # round to get rid of division errors
-    print(f'num interlock cubes vertical: {num_int_vertical}')
-    print(f'height of interlock pattern {int_vertical_height} mm')
+    # print(f'num interlock cubes vertical: {num_int_vertical}')
+    # print(f'height of interlock pattern {int_vertical_height} mm')
     
     # calculate start and end point of interlock pattern (lowest z to highest z)
     center_height = (bounds[2] + cut_height/2)
@@ -177,14 +184,112 @@ def add_cube_interlock(mesh_left, mesh_right, beam_width_layers=2, beam_height_l
 
 
 
+def add_cube_interlock_in_y(mesh_left, mesh_right, beam_width_layers=2, beam_height_layers=2, beam_depth_layers=2, avoidance_dist=NOZZLE_SIZE):
+    """
+    Adds a cube-based interlock between two mesh halves
+    
+    Args:
+        mesh_left: The mesh to have interlock union
+        mesh_right: The mesh to have interlock difference
+        centroid: Centroid of original mesh
+        beam_width_layers: How many layers wide the interlock is. Defaults to 2
+        beam_height_layers: How many layers tall the interlock is. Default is 2
+        beam_depth_layers: How many layer interlock goes into other mesh. Default is 2
+        avoidance_dist: How far interlock is from outer surfaces. Default is the nozzle size
+
+    Returns:
+        Two mesh objects with interlock
+    """
+
+    cut_width, cut_height, bounds = get_split_face(mesh_left)
+
+    min_int_y, max_int_y, min_int_z, max_int_z = (bounds[0] + avoidance_dist,
+                                                  bounds[1] - avoidance_dist,
+                                                  bounds[2] + avoidance_dist,
+                                                  bounds[3] - avoidance_dist)
+    
+    interlock_width = max_int_y - min_int_y
+    interlock_height = max_int_z - min_int_z
+    # print(f'int height: {interlock_height}')
+    # print(f'int width: {interlock_width}')
+
+    num_int_horiz = interlock_width / (beam_width_layers*LAYER_HEIGHT)
+    
+    # if the num is within the tolerance of a cube, then go ahead and add it
+    if np.ceil(num_int_horiz) - num_int_horiz < 0.25:
+        num_int_horiz = np.ceil(num_int_horiz)
+    else:
+        num_int_horiz = np.floor(num_int_horiz)
+
+    # num_int_horiz = np.floor(interlock_width / (beam_width_layers*LAYER_HEIGHT))
+    int_horiz_width = np.round(num_int_horiz * (beam_width_layers*LAYER_HEIGHT), 3) # round to get rid of division errors
+    # print(f'num interlock cubes horiz: {num_int_horiz}')
+    # print(f'width of interlock pattern {int_horiz_width} mm')
+    
+    # calculate start and end point of interlock pattern (lowest z to highest z)
+    center_width = (bounds[0] + cut_width/2)
+    start_width = center_width - (int_horiz_width/2)
+    end_width = center_width + (int_horiz_width/2)
+
+    # print(f'center_width: {center_width}')
+    # print(f'start_width: {start_width}')
+    # print(f'end_width: {end_width}')
+    
+    # define interlock cubes
+    cube_d = beam_depth_layers*NOZZLE_SIZE
+    cube_w = beam_width_layers*LAYER_HEIGHT 
+    cube_h = interlock_height
+    cube_size = [cube_d, cube_w, cube_h] # in mm 
+
+    # hold the cube starting heights to be added/subtracted
+    pattern_start_widths = []
+
+    # populate start heights
+    w = start_width
+    while w < end_width:
+        pattern_start_widths.append(w)
+        w += cube_w
+
+    # plane to add cubes to
+    plane_x = mesh_left.bounds[1][0] # max left of mesh (where the cut was)
+
+    # add/subtract cubes from meshes
+    for i, w in enumerate(pattern_start_widths):
+        cube = trimesh.creation.box(extents=cube_size)
+        
+        # alternate union and diff + translate
+        if i % 2 == 0:
+            cube.apply_translation([plane_x + cube_d/2, w, min_int_z + cube_h/2])
+             # add cube to left mesh
+            mesh_left = trimesh.boolean.union([mesh_left, cube], engine='manifold')
+            # subtract cube to right mesh
+            mesh_right = trimesh.boolean.difference([mesh_right, cube], engine='manifold')
+        else:
+            cube.apply_translation([plane_x - cube_d/2, w, min_int_z + cube_h/2])
+            # subtract cube to left mesh
+            mesh_left = trimesh.boolean.difference([mesh_left, cube], engine='manifold')
+            # add cube to right mesh
+            mesh_right = trimesh.boolean.union([mesh_right, cube], engine='manifold')
+
+    # repair
+    mesh_left.fill_holes()
+    mesh_left.fix_normals()
+    mesh_right.fill_holes()
+    mesh_right.fix_normals()
+
+    return mesh_left, mesh_right
+
+
+
+
 def main():
     # messages will print to console
     trimesh.util.attach_to_log()
 
     mesh = trimesh.load('input_stl/ASTM_D638_TypeIV_Tensile_Test.STL')
-    mesh_left, mesh_right, centroid = split_mesh(mesh)
+    mesh_left, mesh_right = split_mesh(mesh)
 
-    mesh_left_with_interlock, mesh_right_with_interlock = add_cube_interlock(mesh_left, mesh_right, beam_depth_layers=4)
+    mesh_left_with_interlock, mesh_right_with_interlock = add_cube_interlock_in_y(mesh_left, mesh_right, beam_depth_layers=4)
 
 
     mesh_left_with_interlock.export('output/mesh_left.stl')
