589a7197f2a62467aac30f561eb183a7a2afcc0a
KitchNasty02 <connorkitch10@gmail.com>
2026-02-14 15:27:37 -0600

    added 3d dovetail

--- dovetail_interlock_3D.py
+++ dovetail_interlock_3D.py
@@ -5,26 +5,38 @@ import shapely.geometry as geom
 from split_mesh import split_mesh, get_split_face
 
 # Global variables set at Prusa 0.15 mm preset
-NOZZLE_SIZE = 0.4 # mm
-LAYER_HEIGHT = 0.15 # mm
+NOZZLE_SIZE = 0.4  # mm
+LAYER_HEIGHT = 0.15  # mm
 
 
-def add_dovetail_interlock_in_z(mesh_left, mesh_right, taper_angle_deg=10, beam_width_layers=2, beam_height_layers=2, beam_depth_layers=2, avoidance_dist=NOZZLE_SIZE, inverted=False):
+def add_3d_dovetail_interlock(
+    mesh_left,
+    mesh_right,
+    taper_angle_z_deg=10,
+    taper_angle_y_deg=10,
+    beam_width_layers=2,
+    beam_height_layers=2,
+    beam_depth_layers=2,
+    avoidance_dist=NOZZLE_SIZE,
+    inverted=False
+):
     """
-    Adds a dovetail-based interlock between two mesh halves. Centered along seam.
-    
+    Adds a 3D dovetail-based interlock between two mesh halves.
+    Dovetail tapers independently in X and Y directions.
+
     Args:
-        mesh_left: The left half
-        mesh_right: The right half
-        taper_andgle_deg: Angle of taper in degrees. Default is 10
-        beam_width_layers: How many layers wide the interlock is. Defaults to 2
-        beam_height_layers: How many layers tall the interlock is. Default is 2
-        beam_depth_layers: How many layer interlock goes into other mesh. Default is 2
-        avoidance_dist: How far interlock is from outer surfaces. Default is the nozzle size
+        mesh_left: the left half
+        mesh_right: the right half
+        taper_angle_z_deg: taper angle in Z direction
+        taper_angle_y_deg: taper angle in Y direction
+        beam_width_layers: width in Y (layer multiples)
+        beam_height_layers: height in Z (layer multiples)
+        beam_depth_layers: depth in X (layer multiples)
+        avoidance_dist: offset from outer walls
         inverted: which side of dovetail is extruding. Default is false
 
     Returns:
-        Two mesh objects with dovetail interlock
+        mesh_left, mesh_right with interlock
     """
 
     cut_width, cut_height, bounds = get_split_face(mesh_left)
@@ -62,8 +74,8 @@ def add_dovetail_interlock_in_z(mesh_left, mesh_right, taper_angle_deg=10, beam_
 
     
     # define interlock beams
-    dovetail_depth = beam_depth_layers*NOZZLE_SIZE*2
-    dovetail_width = interlock_width        # TODO -- OR (beam_width_layers*NOZZLE_SIZE)
+    dovetail_depth = beam_depth_layers*NOZZLE_SIZE*2    # multiply by 2 since only have of resulting dovetail goes into the other mesh
+    dovetail_width = interlock_width/2        # TODO -- NOT SURE SINCE OTHER END IS BIGGER OR (beam_width_layers*NOZZLE_SIZE)
     dovetail_small_height = beam_height_layers*LAYER_HEIGHT
 
 
@@ -71,11 +83,11 @@ def add_dovetail_interlock_in_z(mesh_left, mesh_right, taper_angle_deg=10, beam_
     # plane to add beams to
     plane_x = mesh_left.bounds[1][0] # max left of mesh (where the cut was)
 
-    dovetail, dovetail_large_height = _create_dovetail_from_taper(dovetail_width, dovetail_small_height, taper_angle_deg, dovetail_depth, inverted)
-    # need to know large height since total height of two dovetails is small_height + large_height
-    print('small trap height: ', dovetail_small_height)
-    print('large trap height: ', dovetail_large_height)
 
+    # create 3D dovetail
+    dovetail, dovetail_z_large_height, dovetail_y_large_width = _create_3D_dovetail_with_taper(
+        taper_angle_z_deg, taper_angle_y_deg, dovetail_small_height, dovetail_width, dovetail_depth, inverted)
+    
     bounds = dovetail.bounds
     dovetail_width_x = bounds[1][0] - bounds[0][0]
     print(f'width: {dovetail_width}, width_x: {dovetail_width_x}')
@@ -92,47 +104,17 @@ def add_dovetail_interlock_in_z(mesh_left, mesh_right, taper_angle_deg=10, beam_
     # center for width (y)
     bounds = dovetail.bounds
     center_y = bounds[:,1].mean()
-    target_y = min_int_y + dovetail_width / 2
+    target_y = min_int_y + dovetail_width 
     dovetail.apply_translation([0, target_y - center_y, 0])
 
-    # hold the beam starting heights to be added/subtracted
-    # pattern_start_heights = []
 
-    # # calculate start heights using alternating large and small side heights
-    # h = start_height
-    # i = 1
-    # while h < end_height:
-    #     if i % 2 == 0:
-    #         pattern_start_heights.append(h)
-    #         h += dovetail_small_height
-
-    #     else:
-    #         # start with large height
-    #         pattern_start_heights.append(h)
-    #         h += dovetail_large_height
-        
-    #     i += 1
+    # scene = trimesh.Scene([mesh_left, dovetail])
+    # scene.show()
 
 
-    # calculate start heights using alternating large and small side heights
-    # TODO -------- MESS WITH THIS TO SEE IF GET RID OF GAP)???
-    # h = start_height
-    # i = 1
-    # while h < end_height:
-    #     if i % 2 == 0:
-    #         pattern_start_heights.append(h)
-    #         h += dovetail_large_height
+    # CALCULATE START HEIGHTS AND UNION/DIFF    
 
-    #     else:
-    #         # start with large height
-    #         pattern_start_heights.append(h)
-    #         h += dovetail_large_height
-        
-    #     i += 1
-
-    # add/subtract dovetails from meshes
-    # for i, h in enumerate(pattern_start_heights):
-    for i, h in enumerate(np.arange(start_height, end_height, dovetail_large_height)):
+    for i, h in enumerate(np.arange(start_height, end_height, dovetail_z_large_height)):
         dovetail_copy = dovetail.copy()
 
         if i % 2 == 0:
@@ -162,24 +144,7 @@ def add_dovetail_interlock_in_z(mesh_left, mesh_right, taper_angle_deg=10, beam_
             # add dovetail to right mesh
             mesh_right = trimesh.boolean.union([mesh_right, dovetail_copy], engine='manifold')
 
-
-    # # add/subtract dovetails from meshes
-    # for i, h in enumerate(pattern_start_heights):
-    #     dovetail_copy = dovetail.copy()
-        
-    #     # alternate union and diff + translate
-    #     if i % 2 == 0:
-    #         dovetail_copy.apply_translation([0, 0, h])
-    #          # add dovetail to left mesh
-    #         mesh_left = trimesh.boolean.union([mesh_left, dovetail_copy], engine='manifold')
-    #         # subtract dovetail to right mesh
-    #         mesh_right = trimesh.boolean.difference([mesh_right, dovetail_copy], engine='manifold')
-    #     else:
-    #         dovetail_copy.apply_translation([0, 0, h])
-    #         # subtract dovetail to left mesh
-    #         mesh_left = trimesh.boolean.difference([mesh_left, dovetail_copy], engine='manifold')
-    #         # add dovetail to right mesh
-    #         mesh_right = trimesh.boolean.union([mesh_right, dovetail_copy], engine='manifold')
+    
 
     # repair
     mesh_left.fill_holes()
@@ -190,54 +155,88 @@ def add_dovetail_interlock_in_z(mesh_left, mesh_right, taper_angle_deg=10, beam_
     return mesh_left, mesh_right
 
 
-
+    
 
 
 
 # helper functions
-def _create_trapezoid(bottom_w, top_w, height):
+def _create_trapezoidal_pyramid(top_width, top_length, bottom_width, bottom_length, height):
+    """
+    Creates a trapezoidal pyramid.
+    
+    Args:
+        top_width: small face width
+        top_length: small face legth
+        bottom_width: large face width
+        bottom_length: large face length
+        height: how tall the pyrimad is
+
+    Returns:
+        Returns trapezoidal pyrimad as a mesh
+    """
 
     vertices = [
-        (-bottom_w/2, 0),
-        (bottom_w/2, 0),
-        (top_w/2, height),
-        (-top_w/2, height)
+        [-bottom_width/2, -bottom_length/2, 0], [bottom_width/2, -bottom_length/2, 0],
+        [bottom_width/2, bottom_length/2, 0], [-bottom_width/2, bottom_length/2, 0],
+        [-top_width/2, -top_length/2, height], [top_width/2, -top_length/2, height],
+        [top_width/2, top_length/2, height], [-top_width/2, top_length/2, height]
     ]
 
-    return geom.Polygon(vertices)
+    # visualize vertices
+    # trimesh.points.PointCloud(vertices=vertices)
+
+    # the number correspond the index of the vertice (counter clockwise)
+    faces = [
+        [0, 1, 5], [0, 5, 4], # Side 1
+        [1, 2, 6], [1, 6, 5], # Side 2
+        [2, 3, 7], [2, 7, 6], # Side 3
+        [3, 0, 4], [3, 4, 7], # Side 4
+        [0, 3, 2], [0, 2, 1], # Bottom
+        [4, 5, 6], [4, 6, 7]  # Top
+    ]
+
+    return trimesh.Trimesh(vertices=vertices, faces=faces)
 
 
-def _create_dovetail_from_taper(width, small_height, taper_angle_deg, depth, inverted=False):
+
+# MAYBE CALCULATE USING Y_LARGE_WIDTH?
+def _create_3D_dovetail_with_taper(taper_angle_z_deg, taper_angle_y_deg, z_small_height, y_small_width, depth, inverted=False):
     """
     Create a dovetail for a given taper angle
     
     Args:
         width: width of bases
         small_height: height of the small connection. Should be at least 2 nozzle width
-        taper_angle_degrees: angle of table based on bottom angle. In degrees
+        taper_angle_deg: angle of table based on bottom angle. In degrees
         depth: depth of dovetail extrude
-        inverted: which side of dovetail is extruding. Small end by default
+        inverted: which side of dovetail is extruding. Default is false
     """
+    z_angle = np.radians(taper_angle_z_deg)
+    z_delta = z_small_height * np.tan(z_angle)  # difference between heights
 
-    angle = np.radians(taper_angle_deg)
+    y_angle = np.radians(taper_angle_y_deg)
+    y_delta = z_small_height * np.tan(y_angle)  # difference between heights
 
-    delta = small_height * np.tan(angle)  # difference between heights
-
-    large_height = small_height + 2 * delta
+    z_large_height = z_small_height + 2 * z_delta
+    y_large_width = y_small_width + 2 * y_delta
+    
     # round to nearest layer height
-    factor = round(large_height / LAYER_HEIGHT)
-    large_height = round(factor * LAYER_HEIGHT, 2)  # round to 2 decimals since layer height is 0.15
+    factor = round(z_large_height / LAYER_HEIGHT)
+    z_large_height = round(factor * LAYER_HEIGHT, 2)  # round to 2 decimals since layer height is 0.15
 
-    trapezoid = _create_trapezoid(small_height, large_height, depth)
-    dovetail = trimesh.creation.extrude_polygon(trapezoid, height=width)
+    factor = round(y_large_width / LAYER_HEIGHT)
+    y_large_width = round(factor * LAYER_HEIGHT, 2)
 
+
+    dovetail = _create_trapezoidal_pyramid(z_small_height, y_small_width, z_large_height, y_large_width, depth) 
+    
     if inverted:
         # rotate small height face to align with cut plane 
         # large end sticking out
         dovetail.apply_transform(
             trimesh.transformations.rotation_matrix(
                 -np.pi / 2,
-                [0, 0, 1]
+                [0, 1, 0]
             )
         )
     else:
@@ -246,17 +245,10 @@ def _create_dovetail_from_taper(width, small_height, taper_angle_deg, depth, inv
         dovetail.apply_transform(
             trimesh.transformations.rotation_matrix(
                 np.pi / 2,
-                [0, 0, 1]
+                [0, 1, 0]
             )
         )
-
-    # rotate so dovetails are horizontal
-    dovetail.apply_transform(
-        trimesh.transformations.rotation_matrix(
-            np.pi / 2,
-            [1, 0, 0]
-        )
-    )
-
-    return dovetail, large_height
+    
+    return dovetail, z_large_height, y_large_width
+    
 
--- stl_slice.py
+++ stl_slice.py
@@ -1,6 +1,7 @@
 from split_mesh import split_mesh
 from beam_interlock import add_beam_interlock_in_y, add_beam_interlock_in_z
 from dovetail_interlock import add_dovetail_interlock_in_z
+from dovetail_interlock_3D import add_3d_dovetail_interlock
 import trimesh
 
 
@@ -12,7 +13,8 @@ def main():
     mesh_left, mesh_right = split_mesh(mesh)
 
     # mesh_left, mesh_right = add_beam_interlock_in_z(mesh_left, mesh_right, beam_depth_layers=4)
-    mesh_left, mesh_right = add_dovetail_interlock_in_z(mesh_left, mesh_right, taper_angle_deg=25, beam_depth_layers=8, beam_height_layers=4, avoidance_dist=0.8)
+    # mesh_left, mesh_right = add_dovetail_interlock_in_z(mesh_left, mesh_right, taper_angle_deg=25, beam_depth_layers=8, beam_height_layers=4, avoidance_dist=0.8, inverted=True)
+    mesh_left, mesh_right = add_3d_dovetail_interlock(mesh_left, mesh_right, 25, 25, beam_depth_layers=4, beam_height_layers=4, avoidance_dist=0.8, inverted=False)
 
 
     mesh_left.export('output/mesh_left.stl')
